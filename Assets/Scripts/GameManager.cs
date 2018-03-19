890f4d0ccc7f1272d241f5494a7bd04fff08d288
mathiasfl <Mathiasflaatt@gmail.com>
2018-03-19 18:20:40 +0100

    Add basic singleton pattern on GameManager class This can be changed later on, as it now are built for the purpose that variables are available in the Unity Inspector. Maybe Lazy loading is a better solution

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int score;
+    public int privateScore;
+    public int dayCount;
+    public int turnCount;
+    private RequirementList requirementList;
+
+    public RequirementList requirements {
+        get { return requirementList; }
+        set { requirementList = value; }
+    }
+
+    private List<StoryEvent> _eventsFired;
+
+    public List<StoryEvent> eventsFired {
+        get { return _eventsFired; }
+        set { _eventsFired = value; }
+    }
+
+
+    //Singleton instanciating
+    public static GameManager Instance { get; private set; }
+
+    private void Awake() {
+        //Check if there are any other instances conflicting
+        if ( Instance != null && Instance != this) {
+            // If that is the case, we destroy other instances
+            Destroy(gameObject);
+        }
+        // Here we save our singleton instance
+        Instance = this;
+        
+        // Makes sure that we don't destroy between scenes
+        DontDestroyOnLoad(gameObject);
+    }
+    
+}
