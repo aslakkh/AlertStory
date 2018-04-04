0b9cc54980ecbc707285d735235a8745a3036255
olejsc <olejsc@stud.ntnu.no>
2018-04-04 16:06:19 +0200

    Added a SettingsController script to the gameobject AppSettingsController Object, to instantiate and controll the apps which appear on the screen. Added a DropDownItemController script for handling event when clicking item in dropdown menu

---
+++ Assets/Scripts/UI-Scripts/DropDownItemController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownItemController : MonoBehaviour {
+
+    public Dropdown settingsDropdown; // The dropdown object in the scene.
+    public Text settingsInformationText; //UI-element object in the scene. Text related to the setting chosen.
+
+    // Dict with the setting name and its description.
+    private Dictionary<string, string> settingsInfo = new Dictionary<string, string>() {
+        {"Private","Private mean noone else can see the actions you do in the app, but yourself" },
+        {"Friends","Friends mean that you and the friends you have in the app can see you actions there."},
+        {"Public", "Public is the setting which makes any other use of the same app, able to see your actions in that app." }
+    };
+
+	// Use this for initialization
+	void Start () {
+        populatelist();
+    }
+	
+
+    //Populates the list of options in the dropdown.
+    void populatelist()
+    {
+        List<string> tempSettings = new List<string>(settingsInfo.Keys);
+        tempSettings.Add("Select option");
+        tempSettings.Reverse(); // reverses the list, so the select option is on top.
+
+        settingsDropdown.captionText.text = "Select option";
+        settingsDropdown.AddOptions(tempSettings);
+
+        settingsInformationText.text = "Please choose a option for the" + " APPNAME HERE" + " app.";
+        settingsInformationText.color = Color.red;
+    }
+
+    // Updates the text corresponding to the option. Dictionairy approach with less if-conditions wasnt supported
+    // in unity's event system.
+    public void settingsDropdown_OnIndexChanged(int index)
+    {
+        if (index == 0)
+        {
+            settingsInformationText.text = "Please choose a option for the" + " APPNAME" + " app.";
+            settingsInformationText.color = Color.red;
+        }
+        else if (index == 1)
+        {
+            settingsInformationText.text = settingsInfo["Private"];
+            settingsInformationText.color = Color.black;
+        }
+        else if (index == 2)
+        {
+            settingsInformationText.text = settingsInfo["Friends"];
+            settingsInformationText.color = Color.black;
+        }
+        else if (index == 3)
+        {
+            settingsInformationText.text = settingsInfo["Public"];
+            settingsInformationText.color = Color.black;
+        }
+        else
+        {
+            settingsInformationText.text = "Please choose a option for the" + " APPNAME HERE" + " app.";
+            settingsInformationText.color = Color.red;
+        }
+    }
+
+    // Validates that the dropbox have a valid option selected.Useful to check before changing scenes. 
+    private bool validateDropDownChoice()
+    {
+        int val = settingsDropdown.value;
+        if (val < 1 && val > 3)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI-Scripts/SettingsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour {
+
+    public Transform AppSettingsPanelPrefab;
+    public Transform BaseCanvasPrefab;
+    public Transform PhoneSettingsInfoPrefab;
+
+    public List<string> appNames = new List<string>();
+    private int currentIndex;
+
+    // Use this for initialization
+    void Start () {
+        currentIndex = 0;
+        var AppSettingsPanel = Instantiate(AppSettingsPanelPrefab);
+        var PhoneSettingsInfo = Instantiate(PhoneSettingsInfoPrefab);
+        AppSettingsPanel.SetParent(BaseCanvasPrefab, false);
+        PhoneSettingsInfo.SetParent(BaseCanvasPrefab,false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
