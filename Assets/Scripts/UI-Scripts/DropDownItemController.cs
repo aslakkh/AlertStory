4e02659bfe80199c7fa00e70bef452d03b14702d
olejsc <olejsc@stud.ntnu.no>
2018-04-09 14:42:57 +0200

    Discarded prefab settup for each app settings. Added scrollbar and viewport for predefined screen with app settings.

--- Assets/Scripts/UI-Scripts/DropDownItemController.cs
+++ Assets/Scripts/UI-Scripts/DropDownItemController.cs
@@ -5,8 +5,26 @@ using UnityEngine.UI;
 
 public class DropDownItemController : MonoBehaviour {
 
-    public Dropdown settingsDropdown; // The dropdown object in the scene.
-    public Text settingsInformationText; //UI-element object in the scene. Text related to the setting chosen.
+    public Dropdown settingsFacebookDropdown;// The dropdown object in the scene.
+    public Dropdown settingsInstabartDropdown;// The dropdown object in the scene.
+    public Dropdown settingsFinnDropdown;// The dropdown object in the scene.
+    public Dropdown settingsTrainerDropdown;// The dropdown object in the scene.
+    public Dropdown settingsSnapDropdown;// The dropdown object in the scene.
+
+
+    public Text settingsFacebookInformationText; //UI-element object in the scene. Text related to the setting chosen.
+    public Text settingsInstabartInformationText; //UI-element object in the scene. Text related to the setting chosen.
+    public Text settingsFinnInformationText; //UI-element object in the scene. Text related to the setting chosen.
+    public Text settingsTrainerInformationText; //UI-element object in the scene. Text related to the setting chosen.
+    public Text settingsSnapInformationText; //UI-element object in the scene. Text related to the setting chosen.
+
+
+    public Text settingFacebookAppNameText; //UI-element object in the scene. Text related to the the current name of the app.
+    public Text settingInstabartAppNameText; //UI-element object in the scene. Text related to the the current name of the app.
+    public Text settingFinnAppNameText; //UI-element object in the scene. Text related to the the current name of the app.
+    public Text settingTrainerAppNameText; //UI-element object in the scene. Text related to the the current name of the app.
+    public Text settingSnapAppNameText; //UI-element object in the scene. Text related to the the current name of the app.
+
 
     // Dict with the setting name and its description.
     private Dictionary<string, string> settingsInfo = new Dictionary<string, string>() {
@@ -17,31 +35,47 @@ public class DropDownItemController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        populatelist();
+        List<string> tempSettings = new List<string>(settingsInfo.Keys);
+        tempSettings.Add("Select option");
+        tempSettings.Reverse();
+        populatelist(settingsFacebookDropdown, tempSettings);
+        populatelist(settingsInstabartDropdown, tempSettings);
+        populatelist(settingsFinnDropdown, tempSettings);
+        populatelist(settingsTrainerDropdown, tempSettings);
+        populatelist(settingsSnapDropdown, tempSettings);
+        settingsFacebookInformationText.text = "Please choose a option for the app.";
+        settingsFacebookInformationText.color = Color.red;
+        settingsInstabartInformationText.text = "Please choose a option for the app.";
+        settingsInstabartInformationText.color = Color.red;
+        settingsFinnInformationText.text = "Please choose a option for the app.";
+        settingsFinnInformationText.color = Color.red;
+        settingsTrainerInformationText.text = "Please choose a option for the app.";
+        settingsTrainerInformationText.color = Color.red;
+        settingsSnapInformationText.text = "Please choose a option for the app.";
+        settingsSnapInformationText.color = Color.red;
     }
 	
 
     //Populates the list of options in the dropdown.
-    void populatelist()
+    void populatelist(Dropdown tempDropdown, List<string> settings)
     {
-        List<string> tempSettings = new List<string>(settingsInfo.Keys);
-        tempSettings.Add("Select option");
-        tempSettings.Reverse(); // reverses the list, so the select option is on top.
-
-        settingsDropdown.captionText.text = "Select option";
-        settingsDropdown.AddOptions(tempSettings);
-
-        settingsInformationText.text = "Please choose a option for the" + " APPNAME HERE" + " app.";
-        settingsInformationText.color = Color.red;
+        tempDropdown.captionText.text = "Select option";
+        tempDropdown.AddOptions(settings);
     }
 
     // Updates the text corresponding to the option. Dictionairy approach with less if-conditions wasnt supported
     // in unity's event system.
     public void settingsDropdown_OnIndexChanged(int index)
     {
+
+        GameObject temp = GameObject.Find("AppSettingFacebookInformationText");
+        Text tempText = temp.GetComponentInChildren<Text>();
+        tempText.text = "Hello world!";
+        Debug.Log("In setting Dropdown On changed menu");
+        /*
         if (index == 0)
         {
-            settingsInformationText.text = "Please choose a option for the" + " APPNAME" + " app.";
+            this.text = "Please choose a option for the app.";
             settingsInformationText.color = Color.red;
         }
         else if (index == 1)
@@ -61,15 +95,16 @@ public class DropDownItemController : MonoBehaviour {
         }
         else
         {
-            settingsInformationText.text = "Please choose a option for the" + " APPNAME HERE" + " app.";
+            settingsInformationText.text = "Please choose a option for the  app.";
             settingsInformationText.color = Color.red;
         }
+        */
     }
 
     // Validates that the dropbox have a valid option selected.Useful to check before changing scenes. 
-    private bool validateDropDownChoice()
+    private bool validateDropDownChoice(Dropdown x)
     {
-        int val = settingsDropdown.value;
+        int val = x.value;
         if (val < 1 && val > 3)
         {
             return false;
--- Assets/Scripts/UI-Scripts/SettingsController.cs
+++ Assets/Scripts/UI-Scripts/SettingsController.cs
@@ -5,26 +5,55 @@ using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour {
 
-    public Transform AppSettingsPanelPrefab;
-    public Transform BaseCanvasPrefab;
-    public Transform PhoneSettingsInfoPrefab;
+    public GameObject AppSettingsPanelPrefab;
+    public GameObject BaseCanvasPrefab;
+    public GameObject PhoneSettingsInfoPrefab;
 
     public List<string> appNames = new List<string>();
     private int currentIndex;
+    private DropDownItemController ddic;
+    private GameObject AppSettingsPanel;
+    private GameObject PhoneSettingsInfo;
 
     // Use this for initialization
     void Start () {
         currentIndex = 0;
-        var AppSettingsPanel = Instantiate(AppSettingsPanelPrefab);
-        var PhoneSettingsInfo = Instantiate(PhoneSettingsInfoPrefab);
-        AppSettingsPanel.SetParent(BaseCanvasPrefab, false);
-        PhoneSettingsInfo.SetParent(BaseCanvasPrefab,false);
+        AppSettingsPanel = Instantiate(AppSettingsPanelPrefab,BaseCanvasPrefab.transform); //Instantiate prefab
+        PhoneSettingsInfo = Instantiate(PhoneSettingsInfoPrefab, BaseCanvasPrefab.transform); //instantiate Prefab
+        //AppSettingsPanel.SetParent(BaseCanvasPrefab, false); //making sure the right canvas object is parrent (phoneprefab)
+        //PhoneSettingsInfo.SetParent(BaseCanvasPrefab,false); //making sure the right canvas object is parrent (phoneprefab)
+        ddic = AppSettingsPanel.GetComponent<DropDownItemController>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    }
 
-    
+    public void moveToNextAppSetting_onClick()
+    {
+        GameObject tempAppSettingsPanel = GameObject.Find("AppSettingsPanel");
+        currentIndex = currentIndex + 1;
+        GameObject.Destroy(tempAppSettingsPanel);
+        AppSettingsPanel = Instantiate(AppSettingsPanelPrefab, BaseCanvasPrefab.transform);
+    }
+
+    public string GetAppSettingName()
+    {
+        GameObject tempAppSettingsPanel = GameObject.Find("AppSettingsPanel");
+        ddic = tempAppSettingsPanel.GetComponent<DropDownItemController>();
+        //return ddic.settingAppNameText.text;
+        return "TODO: Fix SettingsController.cs string return..";
+    }
+
+    public void SetAppSettingName(int index)
+    {
+        GameObject tempAppSettingsPanel = GameObject.Find("AppSettingsPanel");
+        ddic = tempAppSettingsPanel.GetComponent<DropDownItemController>();
+        //ddic.settingAppNameText.text = appNames[index];
+
+    }
+
+
+
+
 }
