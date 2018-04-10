47e2e647b393a4583b2267c1c2b326288b61e752
Aslak Hauglid <aslak@gmail.com>
2018-04-10 13:10:53 +0200

    buttonscontainer script. Allows for disabling of navigation in eventhandlermode

---
+++ Assets/Scripts/Classes/ButtonsContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//add component to buttoncontainers to expose button interactability
+public class ButtonsContainer : MonoBehaviour {
+
+    public List<Button> buttons; //all buttons in this buttoncontainer
+
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.stateChanged += OnStateChanged; //subcribe to stateChanged event
+    }
+
+    public void OnStateChanged(object source, GameStateEventArgs args)
+    {
+        if(args.newState == GameState.investigator)
+        {
+            SetInteractable(true);
+        }
+        else //buttons should only be interactable in investigator state
+        {
+            SetInteractable(false);
+        }
+    }
+
+    public void SetInteractable(bool interactable)
+    {
+        foreach (Button b in buttons)
+        {
+            b.interactable = interactable;
+        }
+    }
+}
