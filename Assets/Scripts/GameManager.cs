11be3bb3ea6739a8299383f6d98c5292b2f241e9
Aslak Hauglid <aslak@gmail.com>
2018-04-10 13:12:55 +0200

    Added a stateChanged event to gamamanager, to notify subscribers of gamestate changes. Updated eventcontrollers, eventmanager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum GameState
@@ -6,6 +7,12 @@ public enum GameState
     investigator,
     eventhandler,
     paused
+}
+
+//Defines eventargs to be sent in the stateChanged event
+public class GameStateEventArgs : EventArgs
+{
+    public GameState newState { get; set; }
 }
 
 public class GameManager : MonoBehaviour {
@@ -16,6 +23,25 @@ public class GameManager : MonoBehaviour {
     public int turnCount;
     private RequirementDict requirementDict;
     private List<StoryEvent> _eventsFired;
+    private EventManager eventManager;
+    private GameState _gameState;
+
+    //gameState property. Publishes event on change
+    private GameState gameState
+    {
+        get { return _gameState; }
+        set
+        {
+            if (_gameState != value)
+            {
+                _gameState = value;
+                OnStateChanged(); //publish event stateChanged
+            }
+        }
+    }
+
+    //event to be published after gamestate change. Contains list of method pointers (subscribers)
+    public event EventHandler<GameStateEventArgs> stateChanged;
 
     //Set this when new state is added.
     public RequirementDict requirements {
@@ -43,18 +69,44 @@ public class GameManager : MonoBehaviour {
         
         // Makes sure that we don't destroy between scenes
         DontDestroyOnLoad(gameObject);
+
+        eventManager = GameObject.Find("EventManager").GetComponent<EventManager>();
+    }
+
+    private void Start()
+    {
+        //TODO: implement proper state flow
+        gameState = GameState.investigator;
     }
 
     public void FireEvent() {
-        //Find list of events and fire first suitable.??
+        bool eventFired = eventManager.InitializeEvent(); //fires first suitable event
+        if (eventFired) { gameState = GameState.eventhandler; }
+        
     }
 
-    public void SetScore(int value)
+    //handles publishing of stateChanged event
+    protected virtual void OnStateChanged()
+    {
+        if(stateChanged != null) //there are subscribers to the event
+        {
+            stateChanged(this, new GameStateEventArgs() { newState = gameState}); //publishes event with new gameState added to eventargs
+        }
+    }
+
+    public void HandleChoice(int score, int privateScore)
+    {
+        AddToScore(score);
+        AddToPrivateScore(score);
+        gameState = GameState.investigator;
+    }
+
+    public void AddToScore(int value)
     {
         score += value;
     }
 
-    public void SetPrivateScore(int value)
+    public void AddToPrivateScore(int value)
     {
         privateScore += value;
     }
