1e7c27be1caaf6fc9f4e202e17dbf5db6d4d08b3
Aslak Hauglid <aslak.hauglid@gmail.com>
2018-04-12 19:49:34 +0200

    main menu, sceneloader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public static SceneLoader Instance { get; private set; } //static singleton
+
+    private void Awake()
+    {
+        //destroy if conflicting instances
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this; //save singleton instance
+
+        DontDestroyOnLoad(gameObject); //persistent in all scenes
+    }
+
+    public void LoadOnClick(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
