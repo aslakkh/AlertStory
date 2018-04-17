002478724598a792bc31d8c5b4d7997ac2c3ad4b
Aslak Hauglid <aslak.hauglid@gmail.com>
2018-04-17 22:07:31 +0200

    friendsbook search in progress

---
+++ Assets/Scripts/Classes/Friendsbook/FriendsbookSearchController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendsbookSearchController : MonoBehaviour {
+
+    /// TODO
+    /// - display/hide search results
+    /// - handle clicks on search element
+    /// - optimization
+    /// 
+
+    public CharacterList characterList; //reference to ScriptableObject containing all in-game characters
+    public InputField searchBar;
+    public FriendsbookSearchView view;
+
+    private List<Character> friendsbookProfiles; //internal reference to all characters with friendsbookProfiles
+
+	// Use this for initialization
+	void Start () {
+        friendsbookProfiles = characterList.list.FindAll(c => c.hasFriendsbookProfile()); //filters out characters without friendsbookProfiles
+	}
+
+    public void Search(string term)
+    {
+        if (!string.IsNullOrEmpty(term))
+        {
+            friendsbookProfiles = characterList.list.FindAll(c => c.fullName.ToLower().Contains(term.ToLower()));
+            view.DisplaySearchResult(friendsbookProfiles);
+        }
+        
+    }
+	
+}
---
+++ Assets/Scripts/Classes/Friendsbook/FriendsbookSearchView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendsbookSearchView : MonoBehaviour {
+
+    public ScrollRect scrollRect;
+    public GameObject searchResultElement;
+
+    private List<GameObject> searchResults;
+
+    private void Start()
+    {
+        searchResults = new List<GameObject>();
+    }
+
+    public void DisplaySearchResult(List<Character> characters)
+    {
+        foreach(GameObject e in searchResults)
+        {
+            GameObject.Destroy(e);
+        }
+        int counter = 0;
+        foreach(Character c in characters)
+        {
+            if(counter >= 10)
+            {
+                break;
+            }
+            string name = c.fullName;
+            GameObject element = Instantiate(searchResultElement, scrollRect.content);
+            element.GetComponentInChildren<Text>().text = name;
+            searchResults.Add(element);
+            counter++;
+        }
+    }
+}
