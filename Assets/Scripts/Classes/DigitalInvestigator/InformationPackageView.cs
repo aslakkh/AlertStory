ebbb8bf69bcac6dd1f7487a96087b880caf8af24
geromath <mathiams@stud.ntnu.no>
2018-04-29 23:53:17 +0200

    just made a few prefabs and empty classes

---
+++ Assets/Scripts/Classes/DigitalInvestigator/InformationPackageView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationPackageView : MonoBehaviour {
+    
+    public Text title;
+    public List<string> informationList = new List<string>();
+    public GameObject currentView;
+    public GameObject informationPackagePrefab;
+    public GameObject rect;
+
+    public void displayInformationPackage() {
+        if (currentView != null) {
+            GameObject.Destroy(currentView);
+        } 
+        currentView = Instantiate(informationPackagePrefab);
+        currentView.transform.SetParent(rect.transform, false);
+
+    }
+
+}
