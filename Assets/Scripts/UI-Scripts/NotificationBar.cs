6ca46dbef26a27abae2f492f7d2e7171f1ce29a8
Eirik Rismyhr <eirikris-95@hotmail.com>
2018-04-30 14:04:25 +0200

    Add settings and scrollview

--- Assets/Scripts/UI-Scripts/NotificationBar.cs
+++ Assets/Scripts/UI-Scripts/NotificationBar.cs
@@ -12,7 +12,6 @@ public class NotificationBar : MonoBehaviour, IPointerClickHandler
     public Text stats;
     public Text objectives;
     public Text settings;
-    //private Image image;
     private List<string> objectivesList;
     private RequirementDict settingsList;
     private Requirement requirement;
@@ -22,37 +21,38 @@ public class NotificationBar : MonoBehaviour, IPointerClickHandler
 
     void Awake()
     {
-        //image = dropDown.GetComponent<Image>();
         dropDown.SetActive(false);
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         stats = GameObject.Find("NotificationBar").transform.GetChild(0).GetComponent<Text>();
         objectives = dropDown.transform.Find("Objectives").GetComponent<Text>();
-        GameManager.Instance.score = 10;
-        GameManager.Instance.objectives = new List<string> { "Objective 1, Objective 2" };
         objectivesList = GameManager.Instance.objectives;
         settingsList = GameManager.Instance.requirements;
     }
 
+    //Opens dropdown
     public void OnPointerClick(PointerEventData eventData)
     {
+        //Sets score string
         stats.text = "Score: " + GameManager.Instance.score.ToString();
-        objectives.text = "Objectives: " + GameManager.Instance.objectives.ToString();
+
+        //Sets objectives string
         StringBuilder objectivesString = new StringBuilder();
         foreach (string objective in objectivesList)
         {
+
             objectivesString.Append(objective + ", " + "\n");
         }
-        objectives.text = "Objectives: " + objectivesString.ToString();
+        objectives.text = "Objectives: " + "\n" + objectivesString.ToString();
 
+        //Sets settings string 
+        settings.text = "Settings: " + "\n";
         StringBuilder settingsString = new StringBuilder();
-
-
-        foreach (KeyValuePair<Requirement, Setting> entry in settingsList.requirementDictionary)
+        foreach (KeyValuePair<string, Setting> item in settingsList.requirementDictionary)
         {
-            settingsString.Append(entry.Value.requirementName + ", " + "\n");
+            settingsString.Append(item.Key + ": " + item.Value + ", " + "\n");
         }
-        settings.text = "Objectives: " + settingsString.ToString();
-
+        settings.text = "Settings: " + "\n" + settingsString.ToString();
+        
         if (dropDown.activeSelf)
         {
             dropDown.SetActive(false);
