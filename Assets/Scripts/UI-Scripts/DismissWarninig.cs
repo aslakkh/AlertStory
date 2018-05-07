05664b6296677df6a03ada5e644fcecd16ac1f1d
mathiasfl <mathiasflaatt@gmail.com>
2018-05-07 15:49:34 +0200

    Add dismiss funtion to Warningpanel

---
+++ Assets/Scripts/UI-Scripts/DismissWarninig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DismissWarninig : MonoBehaviour {
+
+	public GameObject WarningPannel;
+
+	public void HideWarningPanel() {
+		WarningPannel.SetActive(false);
+	}
+}
