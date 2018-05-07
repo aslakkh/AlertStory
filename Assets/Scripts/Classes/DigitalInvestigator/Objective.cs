77a46101583c1fcf04faee1f66d92a6f88c45fa3
Eirik Rismyhr <eirikris-95@hotmail.com>
2018-05-07 15:51:47 +0200

    Information package is shown in dropdown, posts can be added to information package

--- Assets/Scripts/Classes/DigitalInvestigator/Objective.cs
+++ Assets/Scripts/Classes/DigitalInvestigator/Objective.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Tags;
 
 public class Objective : MonoBehaviour {
-
-	
+    private string description;
+    //Tags used to check informationpackage
+    private List<string> tags;
+    //Contains personal info for target person
+    private List<string> personal_info;
 }
