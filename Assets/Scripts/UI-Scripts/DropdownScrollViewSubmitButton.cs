997a55fa17b6851f50034e556145fc957873a3db
Aslak Hauglid <aslak.hauglid@gmail.com>
2018-05-13 15:46:00 +0200

    information package submitted on click

---
+++ Assets/Scripts/UI-Scripts/DropdownScrollViewSubmitButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownScrollViewSubmitButton : MonoBehaviour {
+
+	public void OnClick()
+    {
+        try
+        {
+            //call next day
+            GameObject.Find("GameManager").GetComponent<GameManager>().NextDay(batteryDepleted: false);
+        }
+        catch (System.NullReferenceException e)
+        {
+            Debug.Log(e, this);
+        }
+        
+    }
+}
